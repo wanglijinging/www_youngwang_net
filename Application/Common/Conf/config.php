6c3c0299d27d0a719cb73454fc2be6a14383cbbf
wanglijinging <15201171492@139.com>
2016-05-17 07:55:34 +0800

    clear

--- Application/Common/Conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-	'default_module' => 'Index', //默认模块 
-	'url_model' => '2', //URL模式 
-	'session_auto_start' => true, //是否开启session
-	'__IMAGE__' => __ROOT__ . '/Public/imges'
-);
