715c0ab88d0b3deb1678eab7036becb6ee1885c2
wanglijinging <15201171492@139.com>
2016-08-21 14:49:03 +0800

    ttt

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -15,12 +15,14 @@ return array(
      //'配置项'=>'配置值'  
     'DB_TYPE'      =>  'mysql',     // 数据库类型  
     'DB_HOST'      =>  'localhost',     // 服务器地址  
-    'DB_NAME'      =>  'test',     // 数据库名  
+    'DB_NAME'      =>  'firstproject',     // 数据库名  
     'DB_USER'      =>  'root',     // 用户名  
     'DB_PWD'       =>  '',     // 输入安装MySQL时设置的密码  
     'DB_PORT'      =>  '3306',     // 端口  
-    'DB_PREFIX'    =>  'org_',     // 数据库表前缀  
+    'DB_PREFIX'    =>  'my_',     // 数据库表前缀  
     'DB_DSN'       =>  '',      // 数据库连接DSN 用于PDO方式
     'DEFAULT_MODULE' => 'Home', // 默认模块
-    'PUBLIC_JS_URL' => 'Public/Js'       //公共JS路径
+    'PUBLIC_JS_URL' => 'Public/Js',       //公共JS路径
+    'REDIS_HOST'   => '127.0.0.1',
+    'REDIS_PORT'  => '6379'
 );
